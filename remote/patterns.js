e2229c9ff5962dc9e699c4fb372fe97cfb01788e
Staś Małolepszy <stas@duzodobrze.pl>
2017-04-11 23:27:10 +0200

    Allow punctuation in text patterns

--- remote/patterns.js
+++ remote/patterns.js
@@ -1,11 +1,11 @@
 // vim: ts=4 et sts=4 sw=4
 
 export default {
-    yes: /^(tak|ta|t)$/i,
-    no: /^(nie|ni|n)$/i,
-    cancel: /^nie ?wa[zż]ne$/i,
-    thanks: /^dzi[eę][nm]?[kx](i|s|uje|uję)?$/i,
-    hello: /^(hej|czesc|cześć|ha+lo+)$/i,
-    help: /^(\?|pomocy?|jak.*działa)$/i,
-    start: /^(rozpocznij|start)$/i,
+    yes: /^(tak|ta|t)[.!]*$/i,
+    no: /^(nie|ni|n)[.!]*$/i,
+    cancel: /^nie ?wa[zż]ne[.!]*$/i,
+    thanks: /^dzi[eę][nm]?[kx](i|s|uje|uję)?[.!]*$/i,
+    hello: /^(hej|czesc|cześć|ha+lo+)[.!]*$/i,
+    help: /^(\?|pomocy?|jak.*działa)[.!?]*$/i,
+    start: /^(rozpocznij|start)[.!]*$/i,
 };
